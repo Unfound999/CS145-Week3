4673727e43840a7bd8d5d281106c4e84cdb95f79
Brody Weinkauf <brody.weinkauf@gmail.com>
2025-01-24 15:26:17 -0800

    Added Shape object constructor

---
+++ Shape.java
@@ -0,0 +1,25 @@
+package src;
+
+class Shape {
+
+    //  initializes coordinates
+    private double[] topLeft, bottomLeft, topRight, bottomRight;
+
+    public Shape(double[] topLeft, double[] bottomLeft, double[] topRight, double[] bottomRight) {
+        this.topLeft = topLeft;
+        this.bottomLeft = bottomLeft;
+        this.topRight = topRight;
+        this.bottomRight = bottomRight;
+    } //  ends Shape object constructor method
+
+} //  ends Shape class
+
+class ShapeManager {
+
+}
+
+class ShapeTest {
+    public static void main(String[] args) {
+        
+    }
+}
