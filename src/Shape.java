895ccd5d33cdfaf1b890aa8ad5463848b9ee2801
Brody Weinkauf <brody.weinkauf@gmail.com>
2025-01-28 14:53:41 -0800

    Removed triangle related values from Shape object & removed set and get methods for triangle related values

--- src/Shape.java
+++ src/Shape.java
@@ -4,16 +4,13 @@ class Shape {
 
     //  initializes coordinates
     private double[] topLeft, bottomLeft, topRight, bottomRight;
-    private double triangleHeight, triangleSide;
 
     //  Constructs Shape object type
-    public Shape(double[] topLeft, double[] bottomLeft, double[] topRight, double[] bottomRight, double triangleHeight, double triangleSide) {
+    public Shape(double[] topLeft, double[] bottomLeft, double[] topRight, double[] bottomRight) {
         this.topLeft = topLeft;
         this.bottomLeft = bottomLeft;
         this.topRight = topRight;
         this.bottomRight = bottomRight;
-        this.triangleHeight = triangleHeight;
-        this.triangleSide = triangleSide;
     } //  ends Shape object constructor method
 
     public double[] getTopLeft() {
@@ -32,14 +29,6 @@ class Shape {
         return bottomRight;
     } //  ends getBottomRight method
 
-    public double getTriangleHeight() {
-        return triangleHeight;
-    } //  end getTriangleHeight method
-
-    public double getTriangleSide() {
-        return triangleSide;
-    } //  ends getTriangleSide method
-
     public void setTopLeft(double[] topLeft) {
         this.topLeft = topLeft;
     } //  ends setTopLeft method
@@ -55,14 +44,6 @@ class Shape {
     public void setBottomRight(double[] bottomRight) {
         this.bottomRight = bottomRight;
     } //  ends setBottomRight method
-
-    public void setTriangleHeight(Integer triangleHeight) {
-        this.triangleHeight = triangleHeight;
-    } //  ends setTriangleHeight method
-
-    public void setTriangleSide(Integer triangleSide) {
-        this.triangleSide = triangleSide;
-    } //  ends setTriangleSide method
 } //  ends Shape class
 
 class ShapeManager {
