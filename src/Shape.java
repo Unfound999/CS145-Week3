f9e3dbda9992767aec79b786aa96aa1d485c32af
Brody Weinkauf <brody.weinkauf@gmail.com>
2025-01-28 15:24:04 -0800

    Switched variable order in Shape object & Changed order of set and get methods to improve readability.

--- src/Shape.java
+++ src/Shape.java
@@ -9,26 +9,26 @@ class Shape {
     } //  ends enum
 
     //  initializes coordinates
-    private double[] topLeft, bottomLeft, topRight, bottomRight;
+    private double[] topLeft, topRight, bottomLeft, bottomRight;
 
     //  Constructs Shape object type
-    public Shape(double[] topLeft, double[] bottomLeft, double[] topRight, double[] bottomRight) {
+    public Shape(double[] topLeft, double[] topRight, double[] bottomLeft, double[] bottomRight) {
         this.topLeft = topLeft;
-        this.bottomLeft = bottomLeft;
         this.topRight = topRight;
+        this.bottomLeft = bottomLeft;
         this.bottomRight = bottomRight;
     } //  ends Shape object constructor method
 
     //  methods return called values from Shape object
     public double[] getTopLeft() {return topLeft;}
-    public double[] getBottomLeft() {return bottomLeft;}
     public double[] getTopRight() {return topRight;}
+    public double[] getBottomLeft() {return bottomLeft;}
     public double[] getBottomRight() {return bottomRight;}
 
     //  methods set values in Shape object to the passed values
     public void setTopLeft(double[] topLeft) {this.topLeft = topLeft;}
-    public void setBottomLeft(double[] bottomLeft) {this.bottomLeft = bottomLeft;}
     public void setTopRight(double[] topRight) {this.topRight = topRight;}
+    public void setBottomLeft(double[] bottomLeft) {this.bottomLeft = bottomLeft;}
     public void setBottomRight(double[] bottomRight) {this.bottomRight = bottomRight;}
 } //  ends Shape class
 
