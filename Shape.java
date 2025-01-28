158d43610f5702e9257280b803a6fe26f204baa3
Brody Weinkauf <brody.weinkauf@gmail.com>
2025-01-27 20:49:04 -0800

    Fixed capitalization error in passed variable names in Shape constructor method

--- Shape.java
+++ Shape.java
@@ -4,13 +4,16 @@ class Shape {
 
     //  initializes coordinates
     private double[] topLeft, bottomLeft, topRight, bottomRight;
+    private double triangleHeight, triangleSide;
 
     //  Constructs Shape object type
-    public Shape(double[] topLeft, double[] bottomLeft, double[] topRight, double[] bottomRight) {
+    public Shape(double[] topLeft, double[] bottomLeft, double[] topRight, double[] bottomRight, double triangleHeight, double triangleSide) {
         this.topLeft = topLeft;
         this.bottomLeft = bottomLeft;
         this.topRight = topRight;
         this.bottomRight = bottomRight;
+        this.triangleHeight = triangleHeight;
+        this.triangleSide = triangleSide;
     } //  ends Shape object constructor method
 
     public double[] getTopLeft() {
@@ -29,6 +32,14 @@ class Shape {
         return bottomRight;
     } //  ends getBottomRight method
 
+    public double getTriangleHeight() {
+        return triangleHeight;
+    } //  end getTriangleHeight method
+
+    public double getTriangleSide() {
+        return triangleSide;
+    } //  ends getTriangleSide method
+
     public void setTopLeft(double[] topLeft) {
         this.topLeft = topLeft;
     } //  ends setTopLeft method
@@ -44,6 +55,14 @@ class Shape {
     public void setBottomRight(double[] bottomRight) {
         this.bottomRight = bottomRight;
     } //  ends setBottomRight method
+
+    public void setTriangleHeight(Integer triangleHeight) {
+        this.triangleHeight = triangleHeight;
+    } //  ends setTriangleHeight method
+
+    public void setTriangleSide(Integer triangleSide) {
+        this.triangleSide = triangleSide;
+    } //  ends setTriangleSide method
 } //  ends Shape class
 
 class ShapeManager {
